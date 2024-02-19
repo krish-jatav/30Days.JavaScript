70683b67bbffe7d7925f1944bcfba63606356fba
Krish <krishjatav246@gmail.com>
2024-02-19 13:22:27 +0530

    day3 find largest num

--- day2.js
+++ day2.js
@@ -1,4 +1,5 @@
 // Day 2: Write a program to find the sum of two numbers.
+
 function FuncSum(a, b) {
     let sum = a + b;
     console.log(sum);
---
+++ day3.js
@@ -0,0 +1,9 @@
+// Day 3: Write a program to find the largest number among three numbers.
+
+function largestNum(num1, num2, num3, ) {
+    let largest = Math.max(num1, num2, num3, num4, )
+    return largest;
+
+}
+let result = largestNum(10, 30, 48, );
+console.log("this is a largest number", result)
