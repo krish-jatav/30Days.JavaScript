9dcc1cacb1035d70b6fc393019fd57f09282999a
Krish <krishjatav246@gmail.com>
2024-02-20 22:15:19 +0530

    Day4

---
+++ day4.js
@@ -0,0 +1,11 @@
+// Write a program to reverse a string.
+function reverseString(str) {
+    // Split the string into an array of characters, reverse the array, and join it back into a string
+    return str.split('').reverse().join('');
+}
+
+// Example usage:
+const originalString = "hello";
+const reversedString = reverseString(originalString);
+console.log("Original string:", originalString);
+console.log("Reversed string:", reversedString);
