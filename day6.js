3e4309a84719711ad972e246dac93354876fd67d
Krish <krishjatav246@gmail.com>
2024-02-22 11:31:52 +0530

    2nd commit

--- day6.js
+++ day6.js
@@ -1,12 +1,23 @@
 // Write a program to find the factorial of a number.
 
-let num = 3;
+// let num = 3;
 
-function fact(num) {
-    let ans = 1;
-    if (num === 0)
+// function fact(num) {
+//     let ans = 1;
+//     if (num === 0)
+//         return 1;
+//     for (let i = 2; i <= num; i++) ans = ans * i;
+//     return ans;
+// }
+// console.log(fact(num));
+function fact(n) {
+    if (n === 0 || n === 1) {
         return 1;
-    for (let i = 2; i <= num; i++) ans = ans * i;
-    return ans;
+    } else {
+        return n * fact(n - 1);
+    }
 }
-console.log(fact(num));
+
+let number = 6;
+let result = fact(number)
+console.log(result)
