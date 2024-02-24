89c1ce1948bd774c86164d20ebe3c8c80198962a
Krish <krishjatav246@gmail.com>
2024-02-24 11:00:09 +0530

    find length of string

---
+++ day8.js
@@ -0,0 +1,11 @@
+// Write a program to find the length of a string
+
+let str = "Krish";
+let strinG = str.length
+    // console.log(strinG);
+
+function StringLength(str) {
+    return str.length
+}
+let result = StringLength("krishjatav ")
+console.log("This is String Length", result)
