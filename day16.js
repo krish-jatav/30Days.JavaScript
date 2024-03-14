19243e1833d3d37247aec60beb6b8474c9d13513
Krish <krishjatav246@gmail.com>
2024-03-14 21:26:34 +0530

    remove cmt

--- day16.js
+++ day16.js
@@ -1,10 +1,8 @@
 // Day 16: Write a program to remove duplicate elements from an array.
 
 function removeDuplicates(arr) {
-    // Create a Set to store unique elements
     const uniqueSet = new Set(arr);
 
-    // Convert the Set back to an array
     const uniqueArray = Array.from(uniqueSet);
 
     return uniqueArray;
