a8085c39d629d148da78e8e46d1e155b1eb0ca0d
Krish <krishjatav246@gmail.com>
2024-03-18 13:36:42 +0530

    filter method

---
+++ day22.js
@@ -0,0 +1,34 @@
+// How to Use the filter() Method in JavaScript
+// In the following examples, I will demonstrate how you can use the filter() method to filter items from an array in JavaScript.
+
+// filter() Example 1: How to filter items out of an array
+// In this example, we filter out every person who is a toddler(whose age falls between 0 and 4).
+
+let people = [
+    { name: "aaron", age: 65 },
+    { name: "beth", age: 2 },
+    { name: "cara", age: 13 },
+    { name: "daniel", age: 3 },
+    { name: "ella", age: 25 },
+    { name: "fin", age: 1 },
+    { name: "george", age: 43 },
+]
+
+let toddlers = people.filter(person => person.age <= 3)
+
+console.log(toddlers)
+
+
+
+/*
+[{
+  age: 2,
+  name: "beth"
+}, {
+  age: 3,
+  name: "daniel"
+}, {
+  age: 1,
+  name: "fin"
+}]
+*/
