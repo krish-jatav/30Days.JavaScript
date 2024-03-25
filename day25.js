63dedee0b169826c9645ba761cd66a2a4c73d7de
Krish <krishjatav246@gmail.com>
2024-03-25 20:41:54 +0530

    same output

--- day25.js
+++ day25.js
@@ -1,12 +1,12 @@
 const coding = ["js", "ruby", "java", "python", "cpp"]
 
-coding.forEach(function(val) {
-    console.log(val);
-})
+// coding.forEach(function(val) {
+//     console.log(val);
+// })
 
-// coding.forEach( (item) => {
-//     console.log(item);
-// } )
+coding.forEach((item) => {
+    console.log(item);
+})
 
 // function printMe(item){
 //     console.log(item);
