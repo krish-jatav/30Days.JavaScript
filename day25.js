cad49ac187a74dd90b54c50da37f7991f057d4f2
Krish <krishjatav246@gmail.com>
2024-03-25 20:52:57 +0530

    language name

--- day25.js
+++ day25.js
@@ -9,31 +9,31 @@ const coding = ["apple", "banana", "orange", "mango", "grapes"]
 // })
 
 
-function printMe(item) {
-    console.log(item);
-}
+// function printMe(item) {
+//     console.log(item);
+// }
 
-coding.forEach(printMe)
+// coding.forEach(printMe)
 
-coding.forEach((item, index, arr) => {
-    console.log(item, index, arr);
-})
-
-// const myCoding = [{
-//         languageName: "javascript",
-//         languageFileName: "js"
-//     },
-//     {
-//         languageName: "java",
-//         languageFileName: "java"
-//     },
-//     {
-//         languageName: "python",
-//         languageFileName: "py"
-//     },
-// ]
-
-// myCoding.forEach((item) => {
-
-//     console.log(item.languageName);
+// coding.forEach((item, index, arr) => {
+//     console.log(item, index, arr);
 // })
+
+const myCoding = [{
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach((item) => {
+
+    console.log(item.languageFileName);
+})
