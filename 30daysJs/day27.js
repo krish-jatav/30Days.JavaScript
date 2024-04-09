5e0ba9cf8230c1bac5443f8f90c38581982186b4
Krish <krishjatav246@gmail.com>
2024-04-09 12:20:26 +0530

    30 days js files convert in 1 folder

--- 30daysJs/day27.js
+++ 30daysJs/day27.js
@@ -13,13 +13,13 @@
 // }
 // console.log(str)
 
-let n = 6;
-let str = "";
+// let n = 6;
+// let str = "";
 
-for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-        str += "*";
-    }
-    str += "\n";
-}
-console.log(str)
+// for (let i = 0; i < n; i++) {
+//     for (let j = 0; j < n; j++) {
+//         str += "*";
+//     }
+//     str += "\n";
+// }
+// console.log(str)
--- newFile.js
+++
@@ -1,7 +0,0 @@
-for (let i = 0; i < n; i++) {
-  for (let j = 0; i < n; j++) {
-    // str += "*";
-  }
-
-  // str += "\n";
-}
