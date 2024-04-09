710fe1b942b19f33bbee8809a252e4bf82a1d68e
Krish <krishjatav246@gmail.com>
2024-04-09 12:49:47 +0530

    for loop

---
+++ loops/loops.js
@@ -0,0 +1,31 @@
+// Loops----------
+
+// In programming, loops are control structures used to repeat a block of code multiple times.
+// They are essential for automating repetitive tasks and iterating over collections of data.
+
+// There are several types of loops, but the most common ones are:-
+// for , while , do-while , foreach , forin , forof :
+
+// repetetion of any task
+
+// particular condition
+
+// loops = repeat
+
+// 1 1 1 1 1 1 1
+
+// 1 2 3 4 5 6 7 8
+
+// For loop: Executes a block of code a specified number of times. It typically consists of an initialization step, a condition for continuation, an iteration step, and the code block to execute.
+
+// for (initialization; condition; iteration) {
+//     // code block to be executed
+// }
+
+// for(start;end;change){
+
+// }
+
+// for (var i = 0; i < 5; i++) {
+//     console.log(i);
+// }
