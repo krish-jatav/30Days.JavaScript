1962a539518ba7a218cb8a23be5f2a2ec8bd84f8
Krish <krishjatav246@gmail.com>
2024-04-09 12:05:45 +0530

    2nd pattern star

--- day27.js
+++ day27.js
@@ -2,7 +2,18 @@
 
 // 1. Square Star Pattern in Javascript
 
-let n = 5;
+// let n = 5;
+// let str = "";
+
+// for (let i = 0; i < n; i++) {
+//     for (let j = 0; j < n; j++) {
+//         str += "*";
+//     }
+//     str += "\n";
+// }
+// console.log(str)
+
+let n = 6;
 let str = "";
 
 for (let i = 0; i < n; i++) {
