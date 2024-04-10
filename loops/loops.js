c70d530c4bd9757fd07cbc06a4c59744d3282e7c
Krish <krishjatav246@gmail.com>
2024-04-10 13:22:39 +0530

    Do-while Loop

--- loops/loops.js
+++ loops/loops.js
@@ -26,6 +26,49 @@
 
 // }
 
-// for (var i = 0; i < 5; i++) {
+// for (var i = 5; i < 10; i++) {
 //     console.log(i);
 // }
+
+// while Loop
+
+// while" loop ek control flow statement hai jo ek condition ko evaluate karta hai aur agar wo condition true hoti hai,
+// tab tak ek block of code ko execute karta hai. Jab tak ye condition false nahi ho jati, loop chalta rahta hai.
+
+// start;
+// while(end){
+// change;
+// }
+
+// Yahan "condition" ek boolean expression hoti hai jo evaluate hoti hai.Agar condition true hoti hai, tabhi loop ke andar ka code block execute hota hai.Phir condition firse check hoti hai.Loop tab tak chalta rahega jab tak condition false nahi ho jati hai.
+// Yeh ek simple example hai jisme 1 se 20 tak ke numbers ko print kiya gaya hai:
+
+// let i = 1;
+
+// while (i <= 20) {
+//     console.log(i)
+//     i++;
+// }
+
+
+// // Is example me, loop 1 se start hota hai aur jab tak "i"
+// 20 se chota ya equal hai, loop chalta rahega.iteration me "i"
+// ko 1 se increment kiya jata hai.
+
+// ------------------------------------------------------------------------------
+
+//
+// Do - while loop ek control flow statement hai jo ek block of code ko execute karta hai jab tak ek specified condition true hai.Yeh loop do tarah se kaam karta hai:
+
+// 1.Execution: Sabse pehle, loop ke andar ka code block execute hota hai.
+// 2.Condition Check: Fir, ek condition check hoti hai.Agar ye condition true hoti hai, to loop firse execute hota hai.Agar condition false hoti hai, tab loop terminate ho jata hai.
+
+// let i = 1;
+// let text = "";
+
+// do {
+//     text += "The number is " + i + "\n";
+//     i++;
+
+// } while (i <= 10);
+// console.log(text)
