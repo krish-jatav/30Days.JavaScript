7d90925b73232a3afbe2f2ce4e5a0057836a2ef3
Krish <krishjatav246@gmail.com>
2024-04-11 16:50:34 +0530

    if else concept

---
+++ conditional/conditional.js
@@ -0,0 +1,13 @@
+// conditionals = if else else-if ternary operator switch case
+// The easiest condition is an
+// if statement and its syntax is
+// if (condition) { do this… }.The condition has to be true
+// for the code inside the curly braces to be executed.
+
+var country = "France";
+if (country == "France") {
+    console.log(true)
+} else {
+    console.log(false)
+}
+OUTPUT: false
