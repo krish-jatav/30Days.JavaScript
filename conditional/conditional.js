933e23b54b93cab823e8d10c23127181d89e0f85
Krish <krishjatav246@gmail.com>
2024-04-11 16:57:15 +0530

    program for You are an adult or not

--- conditional/conditional.js
+++ conditional/conditional.js
@@ -4,10 +4,20 @@
 // if (condition) { do this… }.The condition has to be true
 // for the code inside the curly braces to be executed.
 
-var country = "France";
-if (country == "France") {
-    console.log(true)
+// var country = "France";
+// if (country == "France") {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+// OUTPUT: false
+
+// jab bhi apko program mein decision lena ho, k aage ka execution kaisa ho toh  whaha par if else else-if use hota hai
+
+var age = 20;
+
+if (age >= 18) {
+    console.log("You are an adult.");
 } else {
-    console.log(false)
+    console.log("You are not an adult.");
 }
-OUTPUT: false
