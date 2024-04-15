a720c0b1d45dfa1e2f0bd946bfee31fd3f9af641
Krish <krishjatav246@gmail.com>
2024-04-15 12:12:49 +0530

    what is function

--- functions/function.js
+++ functions/function.js
@@ -1,3 +1,7 @@
 // A JavaScript function is a block of code designed to perform a particular task.
 
 // A JavaScript function is executed when "something" invokes it(calls it).
+
+// what is function
+
+// koi esa code jo aapko baar baar use krna ho, ya fir ki aisa code jisko turant na chalana ho par kabhi chalana ho, usey function mein daal skte hai
