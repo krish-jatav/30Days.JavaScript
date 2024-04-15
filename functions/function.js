a26bf688be1963c2f6a0fca938ed087f4132a7eb
Krish <krishjatav246@gmail.com>
2024-04-15 12:30:39 +0530

    paramters and arguments

--- functions/function.js
+++ functions/function.js
@@ -6,7 +6,29 @@
 
 // koi esa code jo aapko baar baar use krna ho, ya fir ki aisa code jisko turant na chalana ho par kabhi chalana ho, usey function mein daal skte hai
 
-function greet(name) {
-    console.log("Hello, " + name + "!");
-}
-greet("krish")
+// function greet(name) {
+//     console.log("Hello, " + name + "!");
+// }
+// greet("krish")
+
+// In this example:
+
+// function is the keyword that declares the function
+// greet is the name of the function.
+// (name) is the parameter that the function accepts.This parameter is optional.
+// {} encloses the block of code that the function executes.This is where the actual logic of the function is written.
+
+// functions - js mein function normal programming language jaise nahi hai, iska matlab hai yaha par js mein , functions banaane ke liyein type nahi batana padta bas aapko function word likhna padega and aap usey koi naam deke bana skte ho
+
+// indepth - js mein functions first class functions ka darja diya gaya hai, jiska matlab hai, ki js mein functions ko value/ variable ki tarah treat kiya ja sakta hai
+
+//  why we need
+
+// to reuse code and to wrap the code which we want to run in future at some point of time
+
+
+// paramters and arguments------------
+
+// Parameters are variables listed in the function definition.
+
+// Arguments are the actual values passed to the function when it is called.
