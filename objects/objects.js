265532996c850fd9789170b824647a18aada2bcc
Krish <krishjatav246@gmail.com>
2024-04-18 11:42:49 +0530

    object program

--- objects/objects.js
+++ objects/objects.js
@@ -4,3 +4,11 @@
 
 // object = ek bande ka kai saara data
 // array = kai logo ka data
+
+let object = {
+    name: "Xyz",
+    age: 20,
+    Enroll: 12345,
+    city: "Bhopal"
+}
+console.log(object.name)
