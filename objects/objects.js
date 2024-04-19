a89afce6fb0f4f12dff99ede5fd195a215e7e8e8
Krish <krishjatav246@gmail.com>
2024-04-19 17:48:48 +0530

    student object

--- objects/objects.js
+++ objects/objects.js
@@ -5,12 +5,21 @@
 // object = ek bande ka kai saara data
 // array = kai logo ka data
 
-let user = {
-    name: "Xyz",
-    age: 20,
-    Enroll: 12345,
-    city: "Bhopal"
-}
-Object.freeze(user);
-user.age = 23;
-console.log(user["age"])
+// let user = {
+//     name: "Xyz",
+//     age: 20,
+//     Enroll: 12345,
+//     city: "Bhopal"
+// }
+// Object.freeze(user);
+// user.age = 23;
+// console.log(user["age"])
+
+// const student = {
+//     name: "krish",
+//     class: 12,
+//     marks: 95.4,
+//     printMarks: function() {
+//         console.log("marks = ", student.marks);
+//     },
+// };
