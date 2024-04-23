97dca3e678dcc3f7e5496a88a18496edb22e8565
Krish <krishjatav246@gmail.com>
2024-04-24 00:22:02 +0530

    arrow function

---
+++ functions/arrowFunc.js
@@ -0,0 +1,5 @@
+// Arrow functions, the basicsThere’s another very simple and concise syntax for creating functions, that’s often better than Function Expressions.
+
+// It’ s called“ arrow functions”, because it looks like this:
+
+// let func = (arg1, arg2, ..., argN) => expression;
