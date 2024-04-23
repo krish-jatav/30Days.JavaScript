e7d0abd85970d942b3b7557f77216d693d818f93
Krish <krishjatav246@gmail.com>
2024-04-24 00:31:15 +0530

    Arrow functions as Function Expressions

--- functions/arrowFunc.js
+++ functions/arrowFunc.js
@@ -14,3 +14,22 @@ let sum = function(a, b) {
 */
 
 // alert(sum(1, 2)); // 3
+
+// As you can see, (a, b) => a + b means a function that accepts two arguments named a and b.Upon the execution, it evaluates the expression a + b and returns the result.
+
+// If we have only one argument, then parentheses around parameters can be omitted, making that even shorter.
+
+// For example:
+
+// let double = n => n * 2;
+// roughly the same as: let double = function(n) { return n * 2 }
+
+// alert(double(3)); // 6
+
+// Arrow functions can be used in the same way as Function Expressions.
+
+// let age = prompt("what is your age..?", 18);
+
+// let welcome = (age > 18) ? () => alert("hello") : () => alert("Greeting");
+
+// welcome();
