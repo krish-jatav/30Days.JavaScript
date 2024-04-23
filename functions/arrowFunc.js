bff6fa170f48e29db7644051717a6fa589e85a1d
Krish <krishjatav246@gmail.com>
2024-04-24 00:23:39 +0530

    This arrow function is a shorter

--- functions/arrowFunc.js
+++ functions/arrowFunc.js
@@ -3,3 +3,14 @@
 // It’ s called“ arrow functions”, because it looks like this:
 
 // let func = (arg1, arg2, ..., argN) => expression;
+
+// let sum = (a, b) => a + b;
+
+/* This arrow function is a shorter form of:
+
+let sum = function(a, b) {
+  return a + b;
+};
+*/
+
+// alert(sum(1, 2)); // 3
