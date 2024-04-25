342c8a79344453d78bc91c110f1d91b5c41d9a56
Krish <krishjatav246@gmail.com>
2024-04-25 18:21:59 +0530

    DOM - Document Object Model

--- functions/arrowFunc.js
+++ functions/arrowFunc.js
@@ -36,9 +36,9 @@ let sum = function(a, b) {
 
 // multiline arrow function
 
-let sum = (a, b) => { // the curly brace opens a multiline function
-    let result = a + b;
-    return result; // if we use curly braces, then we need an explicit "return"
-};
+// let sum = (a, b) => { // the curly brace opens a multiline function
+//     let result = a + b;
+//     return result; // if we use curly braces, then we need an explicit "return"
+// };
 
-alert(sum(1, 2)); // 3
+// alert(sum(1, 2)); // 3
