ce637e340b88936037efa302884e7965c5e4f6f5
Krish <krishjatav246@gmail.com>
2024-04-26 13:47:58 +0530

    What is the DOM ?

---
+++ dom/dom.js
@@ -0,0 +1,6 @@
+// DOM - Document Object Model
+
+// What is the DOM ?
+
+// DOM stands for Document Object Model.It is a programming interface that allows us to create, change, or remove elements from the document.We can also add events to these elements to make our page more dynamic.
+// The DOM views an HTML document as a tree of nodes.A node represents an HTML element.
