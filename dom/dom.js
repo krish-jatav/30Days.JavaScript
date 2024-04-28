9c8bcb1b67a38ec0e754c543f4e38a57fc509314
Krish <krishjatav246@gmail.com>
2024-04-28 13:51:16 +0530

    dom manipulate

--- dom/dom.js
+++ dom/dom.js
@@ -4,3 +4,33 @@
 
 // DOM stands for Document Object Model.It is a programming interface that allows us to create, change, or remove elements from the document.We can also add events to these elements to make our page more dynamic.
 // The DOM views an HTML document as a tree of nodes.A node represents an HTML element.
+
+// kabhi aap chaate ho ki aap ek button ko click krke ek div gaayab karde
+
+// HTML mein koi aisa feature nahi hai jo click ko hanlde kar sake, us js ko hum perform kar skte hai DOM ke saath
+
+// aisa js ka koi bhi code jo webpage mein kuchh bhi change karega wo js ka DOM ka code kahlayega
+
+// DOM = HTML
+
+// DOM Manipulation => body mein kuchh bhi change karna ya manipulate karna actucally DOM Manipulation kehlata hai
+
+// usse ye kardo ye hone par
+
+// selection   change     event
+
+// accessing elements
+
+// ek webpage banao usmein kuch element banao
+
+// selection
+
+// document.querySelector
+// document.getElementById
+// document.getElementsByClassName
+// document.getElementsByTagName
+
+// select & save
+
+// var btn = document.querySelector("button");
+// var heading = document.querySelector("#idName");
